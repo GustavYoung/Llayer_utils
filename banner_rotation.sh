2a725c936adaf484041f180aa63797dde195f057
Gustavo Santana Jordan <gusgus@MacBook-Pro-de-Gustavo.local>
2019-02-09 18:09:59 -0600

    fix 480 9

--- banner_rotation.sh
+++ banner_rotation.sh
@@ -25,7 +25,7 @@ if [ "$resolution" == "$a" ]
 	for entry in $BANNERPATH/*
 	do
 	/home/uslu/Llayer_utils/banner_sleep 3600;
-        /home/uslu/Llayer_utils/bannerimg2 --win '26 37 692 443' -a fill -l -10 -k "$entry";
+        /home/uslu/Llayer_utils/bannerimg2 -l -10 -k "$entry";
 	#sleep 240;
 	done
 fi
@@ -38,16 +38,12 @@ if [ "$resolution" == "$b" ]
         for entry in $BANNERPATH/*
         do
         /home/uslu/Llayer_utils/banner_sleep 3600;
-        /home/uslu/Llayer_utils/bannerimg2 --win '26 37 692 443' -a fill -l -10 -k "$entry";
+        /home/uslu/Llayer_utils/bannerimg2 -l -10 -k "$entry";
         #sleep 240;
         done
 fi
 if [ "$resolution" == "$c" ] 
         while true; do
-        if ps ax | grep -v grep | grep $SERVICE > /dev/null;
-        then
-        sleep 1;
-	else
         for entry in $BANNERPATH/*
         do
         /home/uslu/Llayer_utils/banner_sleep 3600;
