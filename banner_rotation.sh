304a98b0ee5e6eeff7ffe0b95154b960564e9f93
Gustavo Santana Jordan <gusgus@MacBook-Pro-de-Gustavo.local>
2019-02-09 19:57:52 -0600

    fix 480 33

--- banner_rotation.sh
+++ banner_rotation.sh
@@ -17,50 +17,33 @@ BANNERPATH="/home/uslu/elements/banners";
 
 echo "$resolution"
 
-if [ "$resolution" == "$a" ]; then
-        while :; do
-        if ps ax | grep -v grep | grep $SERVICE > /dev/null;
-        then
-        sleep 1;
-	else
-	for entry in $BANNERPATH/*
-	do
-	/home/uslu/Llayer_utils/banner_sleep 3600;
-        /home/uslu/Llayer_utils/bannerimg2 -l -10 -k "$entry";
-	#sleep 240;
-	done
+if [ "$resolution" == "$a" ]
+then
+        boxed="--win '0 0 1920 1080"
 elif [ "$resolution" == "$b" ]; 
 then
-        while :; do
-        if ps ax | grep -v grep | grep $SERVICE > /dev/null;
-        then
-        sleep 1;
-	else
-        for entry in $BANNERPATH/*
-        do
-        /home/uslu/Llayer_utils/banner_sleep 3600;
-        /home/uslu/Llayer_utils/bannerimg2 -l -10 -k "$entry";
-        #sleep 240;
-        done
+        boxed="--win '0 0 1280 720'"
 elif [ "$resolution" == "$c" ]; 
 then
-        while :; do
-        if ps ax | grep -v grep | grep $SERVICE > /dev/null;
-        then
-        sleep 1;
-	else
-        for entry in $BANNERPATH/*
-        do
-        /home/uslu/Llayer_utils/banner_sleep 3600;
-        /home/uslu/Llayer_utils/bannerimg2 --win '26 37 692 443' -a fill -l -10 -k "$entry";
-        #sleep 240;
-        done
+        boxed="--win '26 37 692 443'"
 elif [ "$resolution" == "$d" ]; 
 then
         exit;
 fi
-
-date;
+# infinite loop!
+while true; do
+        if ps ax | grep -v grep | grep $SERVICE > /dev/null
+        then
+        sleep 1;
+else
+	for entry in $BANNERPATH/*
+	do
+	/home/uslu/Llayer_utils/banner_sleep 3600;
+        /home/uslu/Llayer_utils/bannerimg2 "$boxed" -l -10 -k "$entry";
+	#sleep 240;
+	done
+fi
+done
 #./glibimg -b 0 -l 3 Overlay.png &
 #raspivid -t 0
 #killall pngview
